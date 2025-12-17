770c4bf17d8c6d0ac5c9483398e01ddb4d6c3ecd
Rajan Singh <97784760+Rajansinghg@users.noreply.github.com>
2025-12-17 22:43:08 +0530

    Time: 324 ms (6.16%), Space: 47.6 MB (5.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(t.indexOf(s.charAt(i))!= -1){
+                t = t.substring(0,t.indexOf(s.charAt(i))) + t.substring(t.indexOf(s.charAt(i))+1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
