c4ebd1a4b5969566132b4fb5cc5a3fa8ba7ccf0b
Rajan Singh <97784760+Rajansinghg@users.noreply.github.com>
2025-12-18 09:47:15 +0530

    Time: 3 ms (90.06%), Space: 46.6 MB (10.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,28 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length() != t.length()){
+    //         return false;
+    //     }
 
-        for(int i = 0; i < s.length(); i++){
-            if(t.indexOf(s.charAt(i))!= -1){
-                t = t.substring(0,t.indexOf(s.charAt(i))) + t.substring(t.indexOf(s.charAt(i))+1);
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(t.indexOf(s.charAt(i))!= -1){
+    //             t = t.substring(0,t.indexOf(s.charAt(i))) + t.substring(t.indexOf(s.charAt(i))+1);
+    //         }else{
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+     public boolean isAnagram(String s, String t) {
+
+        if(s.length()!= t.length()){return false;}
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        
+        return Arrays.equals(sArray,tArray);
+     }
 }
