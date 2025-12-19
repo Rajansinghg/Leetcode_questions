52121a8f3a68b7544adf8be1a90003cb848ec715
Rajan Singh <97784760+Rajansinghg@users.noreply.github.com>
2025-12-19 17:25:56 +0530

    Time: 0 ms (100%), Space: 43 MB (51.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,30 @@
 class Solution {
+    // public int lengthOfLastWord(String s) {
+    //     int count = 0;
+    //     boolean countReset = false;
+    //     for (int i=0;i<s.length();i++){
+    //         if(s.charAt(i) != ' '){
+    //             if(countReset){
+    //                 count =0;
+    //                 countReset =false;
+    //             }
+    //             count++;
+    //         }else{
+    //             countReset =true;
+    //         }
+    //     }
+    //     return count;
+    // }
+
     public int lengthOfLastWord(String s) {
         int count = 0;
         boolean countReset = false;
-        for (int i=0;i<s.length();i++){
+        for (int i=s.length()-1;i>=0;i--){
             if(s.charAt(i) != ' '){
-                if(countReset){
-                    count =0;
-                    countReset =false;
-                }
                 count++;
-            }else{
-                countReset =true;
+            }
+            else if(count>0 && s.charAt(i) == ' '){
+                break;
             }
         }
         return count;
