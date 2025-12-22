403d5e4500a76cc63e925a0a759069910c330028
Rajan Singh <97784760+Rajansinghg@users.noreply.github.com>
2025-12-22 20:25:11 +0530

    Time: 15 ms (23.15%), Space: 52.6 MB (80.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,25 +4,19 @@ class Solution {
         int rValue = 0;
         int gCount =0;
       for(int num : nums){
-        if(map.containsKey(num)){
-            map.put(num,map.get(num)+1);
-        }else{
-            map.put(num,1);
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
         }
-      }
 
-    //   for (int i=0;i<map.size();i++){
-    //     if(map.get(i)>gCount){
-    //         rValue = map.get(i).getValue();
-    //     }
-    //   }
-
-    for(Map.Entry<Integer,Integer> data : map.entrySet()){
-        if(data.getValue()>gCount){
-            rValue = data.getKey();
-            gCount = data.getValue();
+        for(Map.Entry<Integer,Integer> data : map.entrySet()){
+            if(data.getValue()>gCount){
+                rValue = data.getKey();
+                gCount = data.getValue();
+            }
         }
-    }
     return rValue;
     }
 }
