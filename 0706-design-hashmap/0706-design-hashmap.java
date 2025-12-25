ab5f7fce6e605bd5d451b785b25e5e34f3e87cce
Rajan Singh <97784760+Rajansinghg@users.noreply.github.com>
2025-12-25 12:00:20 +0530

    Time: 32 ms (37.75%), Space: 57.9 MB (11.37%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -2,7 +2,7 @@ class MyHashMap {
     int[] arr;
     public MyHashMap() {
         arr = new int[1000001];
-        Arrays.fill(arr, -1);
+        Arrays.fill(arr,-1);
     }
     
     public void put(int key, int value) {
@@ -14,7 +14,7 @@ class MyHashMap {
     }
     
     public void remove(int key) {
-      arr[key] = -1;
+        arr[key] = -1;
     }
 }
 
